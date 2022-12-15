d460bde8cd8e3b9aa0da843e13f70185a3fb1295
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-15 11:46:48 -0500

    add input number 3

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const { Detail } = require('./classes/Detail');
 //description, price, type, imported
 const details = [];
 
-const product1 = new Product('book', 12.49, 'books', false);
-const detail1 = new Detail(product1, 2);
+//FROM HERE
+const product1 = new Product('imported bottle of perfume', 27.99, 'beauty', true);
+const detail1 = new Detail(product1, 1);
 details.push(detail1);
-const product2 = new Product('music cd', 14.99, 'music', false);
+const product2 = new Product('bottle of perfume', 18.99, 'beauty', false);
 const detail2 = new Detail(product2, 1);
 details.push(detail2);
-const product3 = new Product('choclate bar', 0.85, 'food', false);
+const product3 = new Product('packet of headache pills', 9.75, 'medicine', false);
 const detail3 = new Detail(product3, 1);
 details.push(detail3);
+const product4 = new Product('imported boxes of chocolates', 11.25, 'food', true);
+const detail4 = new Detail(product4, 3);
+details.push(detail4);
 
 
 //UNTIL HERE
